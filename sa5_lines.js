4b30d5844d53d1f783374149152cfcaeb2f3383f
Zach Nicholas <zwnich@gmail.com>
2016-08-07 14:35:04 -0500

    finished exercise 5

--- sa5_lines.js
+++ sa5_lines.js
@@ -3,15 +3,18 @@ var TRstream = through(_tr);
 var split = require('split');
 
 // 
+
+var linecount = 1;
 function _tr(buf, _, callback) {
-	var linecount = 1;
-	line = buf.toString() + '\n';
-	(linecount % 2 === 0) ? this.push(line.toUpperCase()) : this.push(line.toLowerCase());
-	linecount++;
+	var line = buf.toString() + '\n'; 
+	linecount % 2 === 0 
+		 ? this.push(line.toUpperCase())
+		 : this.push(line.toLowerCase());
+  linecount++;
 	callback();
-}
+};
 
 process.stdin
-	.pipe(split());
-	.pipe(TRstream);
+	.pipe(split())
+	.pipe(TRstream)
 	.pipe(process.stdout);
