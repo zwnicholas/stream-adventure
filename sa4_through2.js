9b0ae10a7698124d1360f8bcbec18b48300599c8
Zach Nicholas <zwnich@gmail.com>
2016-08-07 13:28:15 -0500

    --dry-run

--- sa4_through2.js
+++ sa4_through2.js
@@ -1,11 +1,10 @@
-var through = require('through2');
-var TRstream = through(_tr);
+var through2 = require('through2');
+var TRstream = through2(_tr);
 // encoding doesn't appear to even need to be defined
 // learn the difference between the flush function passed to through(...) &
 // the callback signalling the chunk is done being processed by _transform
-function _tr(buf, encoding, callback) {
-	uppercased = buf.toString().toUpperCase();
-	this.push(uppercased);
+function _tr(buf, _, callback) {
+	this.push(buf.toString().toUpperCase());
 	callback();
 }
 process.stdin.pipe(TRstream).pipe(process.stdout);
